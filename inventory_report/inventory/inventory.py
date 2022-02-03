b5671802725eda779be160eeb8eb5e4967b573d3
Caio <caioqs.info@gmail.com>
2022-02-03 17:52:31 -0300

    Requisito 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from ..reports.simple_report import SimpleReport
 from ..reports.complete_report import CompleteReport
 
@@ -7,7 +8,8 @@ class Inventory:
     def import_data(file_name, relatory_type):
         if "csv" in file_name:
             data = read_csv(file_name)
-
+        if "json" in file_name:
+            data = read_json(file_name)
         return relatory(relatory_type, data)
 
 
@@ -21,5 +23,12 @@ def relatory(relatory_type, data):
 def read_csv(file_name):
     with open(file_name) as file:
         data = csv.DictReader(file)
+        new_data = [row for row in data]
+        return new_data
+
+
+def read_json(file_name):
+    with open(file_name) as file:
+        data = json.load(file)
         new_data = [row for row in data]
         return new_data
