95fdb4b149a00f65d9ca0f5d874ff7fb32964c20
Caio <caioqs.info@gmail.com>
2022-02-03 15:57:10 -0300

    Requisito 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,1 +1,23 @@
-# Iniciando prijeto
+from datetime import date
+
+
+class SimpleReport:
+    def generate(data):
+        today = date.today()
+        max_date = []
+        min_date = []
+        stock = []
+
+        for product in data:
+            max_date.append(product["data_de_fabricacao"])
+            if product["data_de_validade"] > today.strftime("%Y-%m-%d"):
+                min_date.append(product["data_de_validade"])
+            stock.append(product["nome_da_empresa"])
+
+        max_stock = max(stock)
+
+        return (
+          f"Data de fabricação mais antiga: {min(max_date)}\n"
+          f"Data de validade mais próxima: {min(min_date)}\n"
+          f"Empresa com maior quantidade de produtos estocados: {max_stock}\n"
+        )
